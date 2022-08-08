7a3839c29a8e013d78d929ef0802430459a1ecc4
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-08 14:07:49 -0500

    task 1 corr 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """async routine called"""
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """list of all the delays (float values)"""
     i = 0
-    list = []
+    list: List[float] = []
     while (i < n):
         value = await (wait_random(max_delay))
         list.append(value)
