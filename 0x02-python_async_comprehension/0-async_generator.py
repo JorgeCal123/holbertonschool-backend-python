a0797edf22f5c7ec8e8d751ea8fa2401cca82788
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-09 13:34:27 -0500

    tsk 0 corr 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,8 +6,8 @@ import random
 
 async def async_generator():
     """return random number between 0 and 10"""
-    i = 0;
-    while( i < 10):
+    i = 0
+    while (i < 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
         i += 1
