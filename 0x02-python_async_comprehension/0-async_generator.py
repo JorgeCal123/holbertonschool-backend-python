97a686da76b4d2ed0e5ef0f286dd6ae18e78aab1
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-09 13:40:01 -0500

    tsk 0 corr 2

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """The coroutine will loop 10 times, each time asynchronously wait 1 second"""
 import asyncio
 import random
-from typing import Float
 
-async def async_generator() -> Float:
+
+async def async_generator():
     """return random number between 0 and 10"""
     i = 0
     while (i < 10):
